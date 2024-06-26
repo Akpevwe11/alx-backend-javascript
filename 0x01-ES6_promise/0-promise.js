a86063b88bf270a58306e733904daecfc966a145
Akpevwe <essiakpevwepraise@gmail.com>
2024-06-26 11:20:08 +0100

    0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,21 +1,4 @@
-/* eslint-disable linebreak-style */
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      const data = 'data';
-      setTimeout(() => {
-        if (data) {
-          resolve(data);
-        } else {
-          reject(new Error('Data not found'));
-        }
-      }, 1000);
-    });
-  }
-  
-  if (require.main === module) {
-    const response = getResponseFromAPI();
-    console.log(response instanceof Promise);
-  }
-
-  module.exports = getResponseFromAPI;
-  
+/* eslint-disable */
+export default function getResponseFromAPI() {
+  return (new Promise((resolve, reject) => {}));
+}
